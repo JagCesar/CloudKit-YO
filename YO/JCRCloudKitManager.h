f61e9787f64e2e742983113cea694f77e4e579a8
César Pinto Castillo <cesar@jagcesar.se>
2014-08-07 20:48:26 -0700

    Creates JCRCloudKitManager

---
+++ YO/JCRCloudKitManager.h
@@ -0,0 +1,13 @@
+//
+//  JCRCloudKitManager.h
+//  YO
+//
+//  Created by César Manuel Pinto Castillo on 07/08/14.
+//  Copyright (c) 2014 JagCesar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JCRCloudKitManager : NSObject
+
+@end
---
+++ YO/JCRCloudKitManager.m
@@ -0,0 +1,13 @@
+//
+//  JCRCloudKitManager.m
+//  YO
+//
+//  Created by César Manuel Pinto Castillo on 07/08/14.
+//  Copyright (c) 2014 JagCesar. All rights reserved.
+//
+
+#import "JCRCloudKitManager.h"
+
+@implementation JCRCloudKitManager
+
+@end
